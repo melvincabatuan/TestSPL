d5342ef6bd01e61f6e7e93109af98538fc92c9f0
melvincabatuan <melvincabatuan@gmail.com>
2020-08-04 08:16:35 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,15 @@
+package ph.edu.dlsu;
+
+import acm.program.ConsoleProgram;
+
+public class Main extends ConsoleProgram {
+
+    public void run() {
+
+        println("LBYCPEI Blank Starter!");
+    }
+
+    public static void main(String[] args){
+        (new Main()).start(args);
+    }
+}
